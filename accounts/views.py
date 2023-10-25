b33ad567c5d314af1adcb8d33d6809c57bd52382
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-10-25 18:07:32 -0500

    Add success and error messages for registration form

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect,render
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -10,10 +11,18 @@ def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            email = email.lower()
             user = form.save(commit=False)
+            user.set_password(password)
+            user.email = email
             user.role = User.CUSTOMER
             form.save()
+            messages.success(request, 'Your account has been registered successfully')
             return redirect('registerUser')
+        else: 
+            print(form.errors)
     else:
         form = UserForm()
     context = {'form':form}
