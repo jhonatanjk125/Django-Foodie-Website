80796bbac21d9cefe2a1fa69a6697b8ee7068200
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-10-25 01:00:05 -0500

    Add custom user models

--- accounts/models.py
+++ accounts/models.py
@@ -38,7 +38,40 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
+    RESTAURANT = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICE = (
+        (RESTAURANT, 'Restaurant'),
+        (CUSTOMER, 'Customer'),
+    )
+
     first_name = models.CharField(max_length=70)
     last_name = models.CharField(max_length=70)
     username = models.CharField(max_length=70, unique=True)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=13, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+
+    # Required fields
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self,perm,obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self,app_label):
+        return True
