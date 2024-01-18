cf44c9dedace4b911b3ce5d2be31d480162fa160
jhonatanjk125 <jhonatanjk125@gmail.com>
2024-01-18 16:32:03 -0500

    Add opening hours model for business

--- vendor/models.py
+++ vendor/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import User, UserProfile
 from accounts.utils import sendNotification
+from datetime import time
 
 # Create your models here.
 class Vendor(models.Model):
@@ -36,3 +37,27 @@ class Vendor(models.Model):
                     
 
         return super().save(*args, **kwargs)
+
+DAYS = [
+    (1, ('Monday')),
+    (2, ('Tuesday')),
+    (3, ('Wednesday')),
+    (4, ('Thursday')),
+    (5, ('Friday')),
+    (6, ('Saturday')),
+    (7, ('Sunday')),
+]
+
+TIME_CHOICES = [(time(h,m).strftime('%I:%M %p'), time(h,m).strftime('%I:%M %p')) for h in range(0,24) for m in(0,30)]
+class OpeningHours(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    day = models.IntegerField(choices=DAYS)
+    from_hour= models.CharField(choices=TIME_CHOICES, max_length=10, blank=True)
+    to_hour = models.CharField(choices=TIME_CHOICES, max_length=10, blank=True)
+    is_closed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('day', 'from_hour')
+        unique_together = ('day', 'from_hour', 'to_hour')
+
+
